95d115c481ba3d05551eba4176847216a73de20c
Samaksh Singh <99samaksh.s@gmail.com>
2024-12-09 10:34:04 +0800

    Updating script
    Performed QC

--- Script/Cell_Hashing.R
+++ Script/Cell_Hashing.R
@@ -21,9 +21,9 @@ setwd("/Users/samaksh/Repositories/Single_Cell_Demultiplexing/Data")
 UBC <- Read10X("./")
 UBC
 
-####################
-# Working with HTO #
-####################
+#########################
+# 3. Observing HTO tags #
+#########################
 
 # HTO labels enable sample multiplexing, doublet detection, and demultiplexing in single-cell hashing experiments.
 
@@ -32,5 +32,25 @@ rownames(HTO) # Checking the presence of HTO labels
 
 # [1] "B0251 anti-human Hashtag1" "B0252 anti-human Hashtag2"
 
+######################
+# 4. Quality Control #
+######################
 
+# Checking the expression matrix to exclude barcode IDs which are 0 in each sample
+ubc.hto <- HTO[colSums(HTO)>0]
+
+UBC.ge <- UBC$`Gene Expression` # extracting gene expression from UBC seurat object
+
+# Intersecting gene expression and antibody capture with barcode ID > 0 
+
+joint.filt_barcodes <- intersect(colnames(UBC.ge), colnames(ubc.hto)) # Obtaining intersection information
+
+UBC.ge <- UBC.ge[, joint.filt_barcodes]
+
+ubc.hto <- as.matrix(ubc.hto[, joint.filt_barcodes])
+
+##############################################
+# 5. Exploratory data analysis and importing #
+# Seurat object for downstream analysis.     #
+##############################################
 
