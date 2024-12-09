953eaf0a6670a06271cf2b823351a518f95444d2
Samaksh Singh <99samaksh.s@gmail.com>
2024-12-09 11:56:28 +0800

    Update Script
    HTO subsetting and standardization

--- Script/Cell_Hashing.R
+++ Script/Cell_Hashing.R
@@ -61,7 +61,89 @@ UBC.tags <- CreateSeuratObject(counts = UBC.ge)
 UBC.tags <- NormalizeData(UBC.tags)
 
 # Identifying features with high variability
-
 UBC.tags <- FindVariableFeatures(UBC.tags, selection.method = "mean.var.plot")
+
+# Scaling seurat object
+UBC.tags <- ScaleData(UBC.tags, features = VariableFeatures(UBC.tags))
+UBC.tags
+
+# An object of class Seurat 
+# 32738 features across 18777 samples within 1 assay 
+# Active assay: RNA (32738 features, 1749 variable features)
+# 3 layers present: counts, data, scale.data
+
+##########################################
+# 6. Adding HTO tag matrix for different #
+# sample source identification.          #
+##########################################
+
+UBC.tags [["HTO"]] <- CreateAssayObject(counts = ubc.hto)
+UBC.tags
+
+# An object of class Seurat 
+# 32740 features across 18777 samples within 2 assays 
+# Active assay: RNA (32738 features, 1749 variable features)
+# 3 layers present: counts, data, scale.data
+# 1 other assay present: HTO
+
+# Central log ratio (CLR) normalization for HTO labels
+UBC.tags <- NormalizeData(UBC.tags,
+                          assay = "HTO",
+                          normalization.method = "CLR")
+
+
+head(UBC.tags@meta.data)
+
+###############################
+# 7. Demultiplexing with HTOs #
+###############################
+
+UBC.tags <- HTODemux(UBC.tags,
+                     assay = "HTO",
+                     positive.quantile = 0.99)
+
+table(UBC.tags$HTO_classification.global)
+
+# Doublet Negative  Singlet 
+# 152    14650     3975
+
+UBC.tags@assays
+
+# $RNA
+# Assay (v5) data with 32738 features for 18777 cells
+# Top 10 variable features:
+#  HBE1, S100A9, CLC, HBD, PF4, PPBP, S100A8, HSPA1A, LYZ, NKG7 
+# Layers:
+#  counts, data, scale.data 
+
+# $HTO
+# Assay data with 2 features for 18777 cells
+# First 2 features:
+#  B0251 anti-human Hashtag1, B0252 anti-human Hashtag2 
+
+# Confirmation for division
+table(UBC.tags$hash.ID)
+
+# Doublet                  Negative                  B0251 anti-human Hashtag1                 B0252 anti-human Hashtag2 
+# 152                     14650                      2144                                      1831 
+
+################################
+# 8. Standardization Operation #
+################################
+
+# Filtering negatives and doublets
+
+UBC.HTO.subset <- subset(UBC.tags,
+                         idents = c("Negative", "Doublet"),
+                         invert = T)
+
+table(Idents(UBC.HTO.subset))
+
+# B0251 anti-human Hashtag1 B0252 anti-human Hashtag2 
+# 2144                      1831 
+
+
+
+
 
 
